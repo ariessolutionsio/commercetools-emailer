8d9db0b25203d6bd4ad073cb5c9ea1169540b2b0
Canopix <canova.9400@gmail.com>
2025-03-19 16:17:33 -0300

    Initial Setup for Emailer UI App

---
+++ custom-application-config.mjs
@@ -0,0 +1,42 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Commercetools Emailer',
+  entryPointUriPath,
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
+  env: {
+    development: {
+      initialProjectKey: '${env:INITIAL_PROJECT_KEY}',
+    },
+    production: {
+      applicationId: '${env:APPLICATION_ID}',
+      url: '${env:APPLICATION_URL}',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  additionalEnv: {
+    logoMustBeVisible: '${env:LOGO_MUST_BE_VISIBLE}',
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
