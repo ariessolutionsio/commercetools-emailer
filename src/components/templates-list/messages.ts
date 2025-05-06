39b8081821b14ae449b59d09ab2102b78eec4ca8
Mauricio <buffamauricio@gmail.com>
2025-05-06 17:02:11 -0300

    remove unused keys

--- src/components/templates-list/messages.ts
+++ src/components/templates-list/messages.ts
@@ -9,20 +9,8 @@ export default defineMessages({
     id: 'loading',
     defaultMessage: 'Loading templates ...',
   },
-  sort: {
-    id: 'sort',
-    defaultMessage: 'Sort',
-  },
-  subject: {
-    id: 'subject',
-    defaultMessage: 'Subject',
-  },
   title: {
     id: 'title',
     defaultMessage: 'Templates',
-  },
-  type: {
-    id: 'type',
-    defaultMessage: 'Type',
   },
 });
