5d995baa44e5a4a7314064615b958febe60bb2b3
Mauricio <buffamauricio@gmail.com>
2025-05-08 16:37:49 -0300

    use a constant for categories

--- src/components/emailer/EmailEditorLayout.tsx
+++ src/components/emailer/EmailEditorLayout.tsx
@@ -2,28 +2,27 @@ import { EmailEditor } from 'easy-email-editor';
 import { StandardLayout } from 'easy-email-extensions';
 import { standardBlocks, layoutBlocks } from './editorConfig';
 
+const CATEGORIES = [
+  {
+    label: 'Content',
+    active: true,
+    blocks: standardBlocks,
+  },
+  {
+    label: 'Layout',
+    active: false,
+    blocks: layoutBlocks,
+  },
+  {
+    label: 'Custom',
+    active: false,
+    blocks: [],
+  },
+];
+
 export const EmailEditorLayout = () => {
   return (
-    <StandardLayout
-      showSourceCode={false}
-      categories={[
-        {
-          label: 'Content',
-          active: true,
-          blocks: standardBlocks,
-        },
-        {
-          label: 'Layout',
-          active: false,
-          blocks: layoutBlocks,
-        },
-        {
-          label: 'Custom',
-          active: false,
-          blocks: [],
-        },
-      ]}
-    >
+    <StandardLayout showSourceCode={false} categories={CATEGORIES}>
       <EmailEditor />
     </StandardLayout>
   );
